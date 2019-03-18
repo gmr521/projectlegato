9d4b0efd2dd533cc50e722aa21a4a08ac87f4294
gmr521 <48675232+gmr521@users.noreply.github.com>
2019-03-18 15:43:07 +0530

    Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,37 @@
+package project1.java;
+
+public class Calculator {
+	int first, second;
+	 
+	/*constructor*/
+	Calculator(int first,int second){
+		 this.first=first;
+		 this.second=second;
+		 
+	 }
+	
+	int addition()
+	{
+		return first+second;
+	}
+	int subtraction()
+	{
+		return first-second;
+	}
+	int multiplication()
+	{
+		return first*second;
+	}
+	int division()
+	{
+		
+			if(this.second==0) {
+				System.out.println("second value cant be zero");
+				return 0;
+			}else {
+				return first/second;
+		}
+		
+	}
+
+}
